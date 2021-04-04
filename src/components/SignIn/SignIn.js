6932d3cbac1f2db63ce35133c373f7930f58946a
Ruslan Valeev <ruvaleev@gmail.com>
2021-04-04 15:57:15 +0700

    SignIn page created

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,66 +1,66 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-// import { useHistory } from 'react-router-dom';
-// import { useTranslation } from 'react-i18next';
-import { Button, Text, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import { StyleSheet, Text, View } from 'react-native';
 
 import withLoading from '../HOC/withLoading';
-// import AuthenticationForm from '../shared/AuthenticationForm';
-// import Errors from '../shared/Errors';
-// import LayoutWithControlPanel from '../shared/LayoutWithControlPanel';
+import AuthenticationForm from '../shared/AuthenticationForm';
+import Errors from '../shared/Errors';
+import LayoutWithControlPanel from '../shared/LayoutWithControlPanel';
 
-// function SignInForm({ onSubmit }) {
-//   const { t } = useTranslation();
+function SignInForm({ onSubmit, navigation }) {
+  const { t } = useTranslation();
 
-//   return (
-//     <LayoutWithControlPanel>
-//       <h1>{t('sign in')}</h1>
-//       <AuthenticationForm onSubmit={onSubmit} />
-//     </LayoutWithControlPanel>
-//   );
-// }
-
-// function SignIn({
-//   isAuthenticated, isError, error, signIn, resetError,
-// }) {
-//   const history = useHistory();
-
-//   useEffect(() => {
-//     if (isAuthenticated) {
-//       history.push('/planner');
-//     }
-//   }, [isAuthenticated]);
-//   return (
-//     <div className="flex flex-col justify-center items-center h-screen">
-//       <Errors isError={isError} error={error} callback={() => resetError()} />
-//       <SignInForm onSubmit={signIn} />
-//     </div>
-//   );
-// }
-
-function SignIn({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sign In page</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    <LayoutWithControlPanel navigation={navigation}>
+      <Text>{t('sign in')}</Text>
+      <AuthenticationForm onSubmit={onSubmit} />
+    </LayoutWithControlPanel>
+  );
+}
+
+function SignIn({ error, isAuthenticated, isError, navigation, resetError, setError, signIn }) {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigation.navigate('Home');
+      setError(t('error:already signed in'));
+    }
+  }, [isAuthenticated]);
+
+  return (
+    <View style={styles.form}>
+      <Errors isError={isError} error={error} callback={() => resetError()} />
+      <SignInForm onSubmit={signIn} navigation={navigation}/>
     </View>
   )
 }
 
 export default withLoading(SignIn);
 
-// SignInForm.propTypes = {
-//   onSubmit: PropTypes.func.isRequired,
-// };
+SignInForm.propTypes = {
+  navigation: PropTypes.object.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
-// SignIn.propTypes = {
-//   resetError: PropTypes.func.isRequired,
-//   signIn: PropTypes.func.isRequired,
-//   error: PropTypes.string,
-//   isError: PropTypes.bool.isRequired,
-//   isAuthenticated: PropTypes.bool.isRequired,
-// };
+SignIn.propTypes = {
+  error: PropTypes.string,
+  isError: PropTypes.bool.isRequired,
+  isAuthenticated: PropTypes.bool.isRequired,
+  navigation: PropTypes.object.isRequired,
+  resetError: PropTypes.func.isRequired,
+  setError: PropTypes.func.isRequired,
+  signIn: PropTypes.func.isRequired,
+};
 
-// SignIn.defaultProps = {
-//   error: null,
-// };
+const styles = StyleSheet.create({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 'calc(100vh - 2rem)',
+    backgroundColor: '#fff',
+  },
+});
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,17 +1,18 @@
 import { connect } from 'react-redux';
 
-import { resetError, signIn } from '../../redux/slices/authentications';
+import { resetError, setError, signIn } from '../../redux/slices/authentications';
 import SignIn from './SignIn';
 
 const mapStateToProps = (state) => ({
-  isLoading: state.authenticationsReducer.isLoading,
-  isError: state.authenticationsReducer.isError,
   error: state.authenticationsReducer.error,
   isAuthenticated: state.authenticationsReducer.isAuthenticated,
+  isError: state.authenticationsReducer.isError,
+  isLoading: state.authenticationsReducer.isLoading,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   resetError: () => dispatch(resetError()),
+  setError: (data) => dispatch(setError(data)),
   signIn: (data) => dispatch(signIn(data)),
 });
 
